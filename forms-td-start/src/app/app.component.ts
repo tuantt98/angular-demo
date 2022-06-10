b77bd87d9d933db18fab73cabaee2f7ca227926a
Tuấn Trần <kisuvitinh@gmail.com>
2022-06-10 14:26:31 +0700

    Angular form (s1)

---
+++ forms-td-start/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  @ViewChild('f') signupForm: NgForm;
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit(){
+    console.log(this.signupForm.form);
+
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+}
