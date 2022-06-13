fa1c132008e7b67910571cffaeb8dc487b1117fa
Tuấn Trần <kisuvitinh@gmail.com>
2022-06-13 10:31:35 +0700

    Angular form (s2)

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,19 +9,53 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
 
   @ViewChild('f') signupForm: NgForm;
+
+  answer = '';
+  genders = ["male", "female"]
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    gender: '',
+    answer: ''
+  }
+  submitted = false
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    })
+    // this.signupForm.form.patchValue({
+    //   userData:{
+    //     username: suggestedName
+    //   }
+    // })
   }
 
+
+  defaultQuestion = 'pet';
   // onSubmit(form: NgForm) {
   //   console.log(form);
   // }
 
-  onSubmit(){
+  onSubmit() {
+    this.submitted = true;
     console.log(this.signupForm.form);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.userData.secret;
+    this.user.gender = this.signupForm.value.gender;
+    this.user.answer = this.answer;
 
+    this.signupForm.reset();
   }
-
   // onSubmit(form: NgForm) {
   //   console.log(form);
   // }
